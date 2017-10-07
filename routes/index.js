5cb35e12b58e9802c7b9600bd886e1383b11e80e
Unknown <sebastian.strindlund@gmail.com>
2017-10-07 17:04:20 +0200

    Design front page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', {
+    title: 'Teamlesser Home',
+    currentYear: new Date().getFullYear(),
+  });
+});
+
+router.get('/chat', function(req, res, next) {
+  res.render('index', {
+    title: 'Teamlesser Chat',
+  });
 });
 
 module.exports = router;
