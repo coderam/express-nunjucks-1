d9ccd86014ba21084033918d107009c4349321ae
Sven Adam Olofsson <adol1601@student.miun.se>
2018-01-11 14:21:30 +0100

    add profile pictures

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,8 +1,10 @@
 let socket = io()
 function Message(props) {
-  return (<div className={props.isCurrentUser ? "current-user" : "other-user"}><div>
-    {!props.isCurrentUser && props.user} {props.message}
-  </div></div>)
+  console.log(props)
+  return (<div className={props.isCurrentUser ? "current-user" : "other-user"}>
+  {props.pic && !props.isCurrentUser && <img className='user-picture' src={'images/' + props.pic}/>}
+  <div>{props.message}</div>
+  </div>)
 }
 class ChatInput extends React.Component {
   state = {
@@ -53,8 +55,8 @@ class Chat extends React.Component {
           {this.state.messages.map(message => 
           <Message
             key={message.id}
+            pic={message.pictureUrl}
             isCurrentUser={this.state.currentUser==message.user}
-            user={message.user ? message.user + ": " : ''}
             message={message.message}
           />
           )}
@@ -74,7 +76,8 @@ class Chat extends React.Component {
           messages : this.state.messages.concat({
             id: data.id,
             user: data.username,
-            message: data.message
+            message: data.message,
+            pictureUrl: data.profilePicUrl
         })
       })
     })
@@ -87,6 +90,7 @@ class Chat extends React.Component {
         })
       })
     })
+    socket.on('add profile picture', profPicUrl => this.setState())
   }
   login = userName => this.setState({currentUser: userName, showLogin: false, showMessages: true}, () => socket.emit('user joined', userName))
   submitMessage = submittedMessage => submittedMessage && socket.emit('chat message', submittedMessage)
@@ -105,99 +109,3 @@ ReactDOM.render(
   <Chat/>,
   document.getElementById('root')
 );
-// function closeChat() {
-//   if (document.getElementById('contentWrapper').classList.contains('hidden')) {
-//     document.getElementById('fnWrap').classList.add('fnWrap');
-//     document.getElementById('fnWrap').classList.remove('close');
-//     document.getElementById('contentWrapper').classList.remove('hidden');
-//     document.getElementById('chatWrapper').classList.remove('hidden');
-//     document.getElementById('chatWrapper').classList.add('chatWrapper');
-//   }
-//   else {
-//     document.getElementById('fnWrap').classList.remove('fnWrap');
-//     document.getElementById('fnWrap').classList.add('close');
-//     document.getElementById('contentWrapper').classList.add('hidden');
-//     document.getElementById('chatWrapper').classList.remove('chatWrapper');
-//     document.getElementById('chatWrapper').classList.add('hidden');
-//   }
-// }
-// document.addEventListener('DOMContentLoaded', function () {
-//   //Login variables
-//   var chatWrap = document.getElementById('chatWrapper');
-//   var loginPage = document.getElementById('loginPage');
-//   var usernameInput = document.getElementById('usernameInput');
-
-//   var messageWrapper = document.getElementById('messagewrapper');
-//   var messages = document.getElementById('messages');
-//   var inputMessage = document.getElementById('chatMessage');
-//   var chatPage = document.getElementById('chatPage');
-//   var chat = document.getElementById('chat');
-//   var headBar = document.getElementById('headBar');
-
-//   var username;
-
-//   usernameInput.addEventListener('keypress', function(e) {
-//     if (e.keyCode === 13) {
-//       if (usernameInput.value) {
-//         username = usernameInput.value;
-//         loginPage.classList.remove('loginPage');
-//         loginPage.classList.add('hidden');
-//         chatPage.classList.remove('hidden');
-//         chat.classList.remove('hidden');
-//         inputMessage.focus();
-//         socket.emit('add user', usernameInput.value);
-//         socket.emit('user joined');
-//       }
-//     }
-//   });
-//   var socket = io();
-
-//   chat.addEventListener('submit', function(e) {
-//     socket.emit('chat message', inputMessage.value, socket.id);
-//     inputMessage.value = '';
-//     e.preventDefault();
-//   });
-
-//   //This is called when a user joins
-//   socket.on('user joined', function (data) {
-//     var joinedUser = document.createElement('li');
-//     joinedUser.innerHTML = data.prefix + data.username + data.postfix;
-
-//     // Determine if we should scroll to bottom after inserting the message.
-//     var atBottom = false;
-//     if ((chatWrap.scrollHeight - chatWrap.scrollTop - chatWrap.clientHeight) < 50) {
-//       atBottom = true;
-//     }
-
-//     // Add the message item to the list and, maybe, scroll to bottom.
-//     document.getElementById('messages').appendChild(joinedUser);
-//     if (atBottom == true) {
-//       chatWrap.scrollTo(0, chatWrap.scrollHeight);
-//     }
-//   });
-
-//   // This is called when the server emits 'chat message', when it has received a message.
-//   // Not called when the client emits 'chat message'.
-//   socket.on('chat message', function(data) {
-//     // Create a new item in the chat list
-//     var item = document.createElement('li');
-//     if (socket.id == data.sId) {
-//       item.classList.add('userWritten');
-//     } else {
-//       item.classList.add('othersWritten');
-//     }
-//     item.innerHTML = data.username + ": " + data.message;
-
-//     // Determine if we should scroll to bottom after inserting the message.
-//     var atBottom = false;
-//     if ((chatWrap.scrollHeight - chatWrap.scrollTop - chatWrap.clientHeight) < 50) {
-//       atBottom = true;
-//     }
-
-//     // Add the message item to the list and, maybe, scroll to bottom.
-//     document.getElementById('messages').appendChild(item);
-//     if (atBottom == true) {
-//       chatWrap.scrollTo(0, chatWrap.scrollHeight);
-//     }
-//   });
-// });
